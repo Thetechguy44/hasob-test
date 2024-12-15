dff9a459916e611f232344548e3f8675dadcb685
DevGana <yahaya128173908@gmail.com>
2024-12-15 10:39:08 +0100

    clear cache

--- dmo-savings-bond-portal/storage/framework/views/6c708a4c06a653f67c1c58d3e6fe822d212204f3.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php $__env->startSection('body'); ?>
-
-    <?php echo $__env->yieldContent('content'); ?>
-    
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.gradebook-frontend-blue.frontend', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\DocB\Desktop\workspace\scola-gradebook-portal\resources\views/layouts/frontend.blade.php ENDPATH**/ ?>
